b0d38bbcc8b5fa7f98d1e2f9140a3b41352231e5
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2026-04-25 01:42:29 -0300

    perfil controller calificar perfil again

--- src/main/java/app/controllers/PerfilController.java
+++ src/main/java/app/controllers/PerfilController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
@@ -35,14 +36,15 @@ public class PerfilController {
         return ResponseEntity.ok(operaciones);
     }
 
-    @PostMapping("/{user_id}/calificaciones")
+    @PostMapping("/{perfil_id}/calificaciones")
     public ResponseEntity<TemporalDto> calificarPerfil(
-        @PathVariable String user_id,
+        @PathVariable String perfil_id,
+        @RequestHeader String autor_id,
         @RequestBody Map<String, Object> body) {
 
         Number calificacionMedia = this.perfilService.agregarCalificacion(
-            (String) body.get("autor_id"),
-            user_id,
+            autor_id,
+            perfil_id,
             (Integer) body.get("valor"),
             (String) body.get("descripcion")
         );

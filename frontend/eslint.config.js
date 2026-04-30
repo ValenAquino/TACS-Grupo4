ba8fc68aa266a1253b0e171e4054d0979b3689e0
ValenAquino <vaquino@frba.utn.edu.ar>
2026-04-30 17:29:40 -0300

    agregar configuración de `eslint-plugin-react` y nuevas reglas de linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,12 +11,24 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-no-duplicate-props': 'error',
+      'no-console': 'warn',
     },
   },
 ])

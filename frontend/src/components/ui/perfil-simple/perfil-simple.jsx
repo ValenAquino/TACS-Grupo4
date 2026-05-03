bf36df97466d42e91d5526388c9b4657152c9989
LucasFis <lfiscella@frba.utn.edu.ar>
2026-05-03 02:57:22 -0300

    Changes: Mejoro visualmente las estrellas

--- frontend/src/components/ui/perfil-simple/perfil-simple.jsx
+++ frontend/src/components/ui/perfil-simple/perfil-simple.jsx
@@ -4,12 +4,28 @@ import {useNavigate} from "react-router";
 const PerfilSimple = ({perfil}) => {
 
     const navigate = useNavigate()
+
+    const renderStars = (rating) => {
+        return (
+            <div className={styles.stars}>
+                {[1,2,3,4,5].map((i) => (
+                    <span key={i} className={i <= rating ? styles.filled : styles.empty}>
+                    ★
+                </span>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div className={styles.avatarContainer + " d-flex align-items-center gap-2"} onClick={() => navigate(`/perfil/${perfil.id}`)}>
             <div className={styles.avatar}>{perfil.nombre.slice(0,1).toUpperCase()}</div>
             <div>
                 <h5 className="m-0">{perfil.nombre}</h5>
-                <p className="fs-6">Puntuacion media: {perfil.puntuacion}</p>
+                <p className="fs-6 d-flex align-items-center gap-2">
+                    {renderStars(perfil.puntuacion)}
+                    <span>{perfil.puntuacion}</span>
+                </p>
             </div>
         </div>
     )

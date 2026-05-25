b9169dbac3ecffabe7443f95e4ffae853058f375
ValenAquino <vaquino@frba.utn.edu.ar>
2026-05-25 18:58:54 -0300

    añadir `imagenUrl` a `FiguritaIntercambiableDto`

--- backend/src/main/java/app/dto/FiguritaIntercambiableDto.java
+++ backend/src/main/java/app/dto/FiguritaIntercambiableDto.java
@@ -4,23 +4,23 @@ import app.model.entities.FiguritaIntercambiable;
 import app.model.entities.MetodoIntercambio;
 import app.model.entities.Perfil;
 import app.model.entities.Seleccion;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class FiguritaIntercambiableDto {
-  private String figuritaId;
-  private Integer numero;
-  private String jugador;
-  private Seleccion seleccion;
-  private Integer cantidadExistente;
-  private Integer cantidadReservada;
-  private List<MetodoIntercambio> metodos;
-  private String perfilId;
-  private String nombreUsuario;
-  private Integer reputacion;
-  private String posicion;
+  private final String figuritaId;
+  private final Integer numero;
+  private final String jugador;
+  private final Seleccion seleccion;
+  private final Integer cantidadExistente;
+  private final Integer cantidadReservada;
+  private final List<MetodoIntercambio> metodos;
+  private final String perfilId;
+  private final String nombreUsuario;
+  private final Integer reputacion;
+  private final String posicion;
+  private final String imagenUrl;
 
   public FiguritaIntercambiableDto(FiguritaIntercambiable f, Perfil perfil) {
     this.figuritaId = f.getFigurita().getId();
@@ -28,6 +28,7 @@ public class FiguritaIntercambiableDto {
     this.jugador = f.getFigurita().getJugador();
     this.posicion = f.getFigurita().getPosicion();
     this.seleccion = f.getFigurita().getSeleccion();
+    this.imagenUrl = f.getFigurita().getImagenUrl();
     this.cantidadExistente = f.getCantidadExistente();
     this.cantidadReservada = f.getCantidadReservada();
     this.metodos = f.getMetodos();

29eefac95088a804c896eb30ec8cfa8f2cd6a820
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2026-05-25 20:23:43 -0300

    inicializador, figurtas, elimino posicion

--- backend/src/main/java/app/model/entities/Seleccion.java
+++ backend/src/main/java/app/model/entities/Seleccion.java
@@ -1,5 +1,44 @@
 package app.model.entities;
 
 public enum Seleccion {
-  ARGENTINA, FRANCIA, BRASIL, ESPAÑA, ALEMANIA
+  // Grupo A
+  QATAR,
+  ECUADOR,
+  SENEGAL,
+  PAISES_BAJOS,
+  // Grupo B
+  INGLATERRA,
+  IRAN,
+  EEUU,
+  GALES,
+  // Grupo C
+  ARGENTINA,
+  ARABIA_SAUDITA,
+  MEXICO,
+  POLONIA,
+  // Grupo D
+  FRANCIA,
+  AUSTRALIA,
+  DINAMARCA,
+  TUNEZ,
+  // Grupo E
+  ESPAÑA,
+  COSTA_RICA,
+  ALEMANIA,
+  JAPON,
+  // Grupo F
+  BELGICA,
+  CANADA,
+  MARRUECOS,
+  CROACIA,
+  // Grupo G
+  BRASIL,
+  SERBIA,
+  SUIZA,
+  CAMERUN,
+  // Grupo H
+  PORTUGAL,
+  GHANA,
+  URUGUAY,
+  COREA_DEL_SUR
 }

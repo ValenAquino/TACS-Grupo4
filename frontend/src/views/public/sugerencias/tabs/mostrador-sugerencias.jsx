c7ef7a9eab47a724ed046c3f4b6c1dd18266d52e
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2026-05-26 14:40:57 -0300

    elimino useUsuarioActual

--- frontend/src/views/public/sugerencias/tabs/mostrador-sugerencias.jsx
+++ frontend/src/views/public/sugerencias/tabs/mostrador-sugerencias.jsx
@@ -3,7 +3,6 @@ import {useCallback, useEffect, useState} from "react";
 import {buscarSugerencias} from "@/services/perfilService.js";
 import ExtraInfo from "@/components/ui/extra-info/extra-info.jsx";
 import Paginacion from "@/components/ui/paginacion/paginacion.jsx";
-import useUsuarioActual from "@/hooks/useUsuarioActual.js";
 
 const MostradorSugerencias = ({tipo, extraInfoChildren}) => {
 
@@ -12,7 +11,6 @@ const MostradorSugerencias = ({tipo, extraInfoChildren}) => {
     const [sugerencias, setSugerencias] = useState([])
     const [pagina, setPagina] = useState(1)
     const [paginasTotales, setPaginasTotales] = useState(1)
-    const {userId} = useUsuarioActual()
 
     const cargarSugerencias = useCallback(async () => {
         try {

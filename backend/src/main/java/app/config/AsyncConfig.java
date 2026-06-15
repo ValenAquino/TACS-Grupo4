703b208c66d16ee16544786c307e24aaf4c80484
ValenAquino <vaquino@frba.utn.edu.ar>
2026-06-15 14:20:56 -0300

    se agregan metricas del executor de agregar-datos-de-figuritas

--- backend/src/main/java/app/config/AsyncConfig.java
+++ backend/src/main/java/app/config/AsyncConfig.java
@@ -1,5 +1,8 @@
 package app.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -8,7 +11,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@RequiredArgsConstructor
 public class AsyncConfig {
+
+  private final MeterRegistry meterRegistry;
 
   @Bean
   public TaskExecutor taskExecutor() {
@@ -18,6 +24,11 @@ public class AsyncConfig {
     executor.setQueueCapacity(100);
     executor.setThreadNamePrefix("agregar-datos-de-figuritas-");
     executor.initialize();
+
+    ExecutorServiceMetrics.monitor(
+        meterRegistry, executor.getThreadPoolExecutor(), "agregar-datos-de-figuritas"
+    );
+
     return executor;
   }
 }
